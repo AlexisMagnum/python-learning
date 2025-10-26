8bfdac5a6b43d084379132534f4b188e693fa771
D3nst0n D3nst0n <d3nst0n1448@gmail.com>
2025-10-26 12:12:16 +0500

    4 изменение

---
+++ hello.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+	CREATE IF NOT EXISTS TABLE products (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT
+	)
+	''')
